7c0a0e4c7da94951f06942d8f4a410b92b363534
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2022-01-25 08:47:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-function towelSort (matrix) {
+function towelSort (mat) {
 //     работает, но тест не проходит
-//   for(let rowIndex in matrix){
-//     matrix[rowIndex].sort((a, b) => rowIndex % 2 === 0 ? a-b : b-a)
-//   }
-//  return matrix.flat()
+   for(let rowIndex in mat){
+     mat[rowIndex].sort((a, b) => rowIndex % 2 === 0 ? a-b : b-a)
+   }
+  return mat.flat()
     
-  const res=[]
-  for(let i=0; i<matrix.length; i++){
-    if(i%2 === 0) res.push(...[...matrix[i]].sort((a, b)=> a-b))
-    else res.push(...[...matrix[i]].sort((a, b)=> b-a))
-  }
- return res
+//   const res=[]
+//   for(let i=0; i<matrix.length; i++){
+//     if(i%2 === 0) res.push(...[...matrix[i]].sort((a, b)=> a-b))
+//     else res.push(...[...matrix[i]].sort((a, b)=> b-a))
+//   }
+//  return res
 }
