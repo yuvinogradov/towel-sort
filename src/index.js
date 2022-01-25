4c88858de2c98f029d4fb39d8886ed70e877a7b5
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2022-01-25 07:56:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return matrix = matrix.flat().sort((a,b)=>a-b);
+  return matrix.flat().sort((a,b)=>a-b);
 }
