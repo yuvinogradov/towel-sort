83199a44242bf5fd5c380b46bbb797075f225e7f
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2022-01-25 08:10:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function towelSort (matrix) {
-  for(rowIndex in matrix){
+  for(let rowIndex in matrix){
     matrix[rowIndex].sort((a, b) => rowIndex % 2 === 0 ? a-b : b-a)
   }
  return matrix.flat()
